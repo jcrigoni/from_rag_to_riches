9417f3cc1eb598b5bac73ef909d51f52cb783247
jcrigoni <jcrigoni@gmail.com>
2025-04-24 13:21:25 +0200

    Add rag_graph script

--- rag_project/core/rag_graph.py
+++ rag_project/core/rag_graph.py
@@ -1,4 +1,4 @@
-"""Module for LangGraph-based RAG implementation. from_rag_to_riches"""
+"""Module for LangGraph-based RAG implementation. """
 
 from typing import TypedDict, Annotated, List, Union
 from langchain_core.messages import HumanMessage, AIMessage
